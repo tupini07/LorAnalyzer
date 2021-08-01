97df475600486b19e8574e1ac23d00e362507ba0
Andrea Tupini <tupini07@gmail.com>
2021-08-01 13:09:34 -0600

    fix issue with require

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { downloadDataIfNecessary } from "./downloader";
 downloadDataIfNecessary().then(() => {
   // we first need to check and download data before actually executing
   // application logic
-  const runCli = require("./cli");
-  runCli();
+  const cli = require("./cli");
+  cli.runCli();
 });
